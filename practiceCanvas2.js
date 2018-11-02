f4fe725432dcd846e75240be82cfdad14b7238bb
Neeko Blomgren <>
2018-11-02 15:28:51 -0700

    Added Movement speed, when added the Re-render button now seems to only render half of the total circles, and only the ones with decreasing Y values???

--- practiceCanvas2.js
+++ practiceCanvas2.js
@@ -1,3 +1,153 @@
+/*
+Neeko Blomgren
+November 2nd, 2018
+The Ballpit - Canvas Animation Ballpit Simualor
+
+
+    This program is built keeping best practices in mind:
+    - Highly Minimized global variables. 
+    - Encapsulated functionality.
+    - ES6 Syntax and prioritizing block scope.
+    - Using closures and getters and setters to maximize privacy and minimize naming collisions
+
+    Variable Naming Conventions:
+        Inside of the initialization function, theContext and theCanvas are designations 
+    representing their being initialized within that scope. In other methods such as shapesModule(),
+    clear(), and resizeCanvas(), c and ctx are designations representing them being passed arguments within
+    those respective scopes.  
+
+    
+    Note from author:
+        All code is 100% written by me, no copy and pasting from online solutions, 
+        and no tutorials directly followed. Referenced documentation for certain features
+        such as canvas and websites like stackoverflow for questions about syntax and reasoning thru problems.
+
+
+    Notes:
+        - The interval object is very important here. Keep track of where it goes, when it setIntervals()s, and when 
+        it clearInterval()s. The same interval instance can be passed from function to function.
+*/
+
+
+const onPageLoad = () =>
+{
+    // Declaration of interval object
+    let interval;
+
+    // // Receives HTML elements into script
+    // const amtInput = document.getElementById('amtInput');       // why don't i need these lines?
+    // const radInput = document.getElementById('radInput');
+    // const spdInput = document.getElementById('spdInput');
+
+    // Binding event handler to Render Button
+    document.getElementById('renderer').onclick = (e) =>  { interval = render(interval); };
+
+    // Binding event handler to Randomize Button
+    document.getElementById('randomer').onclick = (e) =>  { interval = randomize(interval) };
+
+    // Initial Default Page Load
+    interval = init();
+}
+
+    // Render function which takes the interval object as an argument. This allows for the same interval being passed around,
+    // being able to be set and cleared whenever a new initialization is set.
+    const render = (int) => 
+    {
+        // Clears previous 
+        clearInterval(int);
+        
+        // Gathers values from elements
+        let newAmt = amtInput.value;
+        let newRad = radInput.value;            // Note: how is this render function able to access variables from onPageLoad? does it have something to do with the event handler?
+                                            // im confused. does the input html element automatically make itself available
+                                            // in my script scope just by being instantiated?
+        let newSpd = spdInput.value;
+
+
+        // Passes new values into initialization script
+        int = init(newAmt,newRad,newSpd);
+        return int;
+    }
+    // Same as above, but with randomization
+    const randomize = (int) =>
+    {
+        // Clears previous 
+        clearInterval(int);
+
+        // Randomizes amount and radius coefficient
+        let newAmt = Math.random() * 500;
+        let newRad = Math.random() * 100;
+        let newSpd = Math.random() * 4
+
+        // Putting (rounded) new values into the HTML element for display.
+        amtInput.value = Math.floor(newAmt);
+        radInput.value = Math.floor(newRad);
+        spdInput.value = newSpd;
+
+        int = init(newAmt,newRad,newSpd);
+        return int;
+    }
+
+// Initialization Script
+const init = (amt,rad,spd) =>
+{
+    // Declaration for 
+    let amtCirc = amt;
+    let radCo = rad;
+    let movSpd = spd
+
+    // Default. If the two variables are found to be undefined (as with the initial page load), their default values are set to 100 and 50
+    if (!amtCirc && !radCo && !movSpd)
+    {
+        amtCirc = 100; radCo = 50; movSpd = 1;
+    }
+
+    // Init of Canvas
+    const theCanvas = document.getElementById('myCanvas');
+    const theContext = theCanvas.getContext('2d');
+
+    // Resize to Window Viewport on pageload
+    resizeCanvas(theCanvas);
+
+    
+    // Placing Context and Canvas into my shapes modules to encapsulate it for usage with theShapes object instance
+    shapes = shapesModule(theContext,theCanvas);
+
+
+        // Animation Time!!!
+    // Generates Our Circles
+    let circles = genCircles(amtCirc,radCo,movSpd,shapes,theCanvas);
+
+    // Animate function sets recursive draw() calls
+    const animate = () =>
+    {
+        return setInterval(draw, 10);
+    }
+    
+    // Draws scene once, calls CircleDrawLoop where implementation of bouncing circles is defined
+    const draw = () =>
+    {
+        clear(theContext,theCanvas);
+        circleDrawLoop(circles, theCanvas);
+    }
+
+    // Returns interval object to scope which called it.
+    return animate();
+}
+
+    // Resizes canvas to current window viewport
+    const resizeCanvas = (c) => 
+    {
+        c.width = window.innerWidth;
+        c.height = window.innerHeight;
+    }
+    // Clear Canvas
+    const clear = (ctx,c) =>
+    {
+        ctx.clearRect(0,0,c.width,c.height);
+    }
+
+
 
 // Different Shape Classes
 const shapesModule = (ctx,c) =>
@@ -43,30 +193,25 @@ const shapesModule = (ctx,c) =>
             }
         },
 
-        Circle: (newX,newY,newR) => 
+        Circle: (newX,newY,newR,newMoveX,newMoveY) => 
         {
             let x = newX;
             let y = newY;
             let r = newR;
 
-            let moveX; let moveY;
+            // Movement Variables, stores a value of potential displacement on draw()
+            let moveX = newMoveX; 
+            let moveY = newMoveY;
 
                 // Randomizes initial movement direction
             let posOrNeg = Math.random() < 0.5 ? -1 : 1;
 
             if(posOrNeg > 0)
             {
-                moveX = 1;
-                moveY = 1;
+                moveX *= -1;
+                moveY *= -1;
             }   
-            else 
-            {
-                moveX = -1;
-                moveY = -1;
-            }
-
-
-
+            
             return {
                 draw()
                 {
@@ -75,7 +220,7 @@ const shapesModule = (ctx,c) =>
                         ctx.beginPath();
                         ctx.arc(x,y,r, 0, 2 * Math.PI);
                         ctx.stroke();
-                    } else throw console.error("Arguments Not Numbers!");
+                    } 
 
                 },
 
@@ -97,40 +242,31 @@ const shapesModule = (ctx,c) =>
         }
     }
 }
-        // Resizes canvas to current window viewport
-        const resizeCanvas = (canvas) => 
-        {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-        }
-        // Clear Canvas
-        const clear = () =>
-        {
-            const canvas = document.getElementById('myCanvas');
-            const context = canvas.getContext('2d');
-            context.clearRect(0,0,canvas.width,canvas.height);
-        }
-
         // Circle Generator, takes number of Circle objects to return, shapes modules, and current canvas as arguments
-        const genCircles = (num,rad,shapes,canvas) =>
+        const genCircles = (num,rad,spd,shapes,c) =>
         {
             let circles = [];
-            let x; let y; 
+            let x; let y; let r; let s;
 
+            console.log(spd);
+            
             for (let i = 0; i < num; i++)
             {
                 r = 1 + (Math.random() * rad);
+                x = r + (Math.random() * (c.width - (2*r)));
+                y = r + (Math.random() * (c.height - (2*r)));
+                s = spd;
 
-                x = r + (Math.random() * (canvas.width - (2*r)));
-                y = r + (Math.random() * (canvas.height - (2*r)));
-                circles.push(shapes.Circle(x,y, r));
+                circles.push(shapes.Circle(x,y,r,s,s));
             }
 
+            
             return circles;
+
         }
 
         // Circle Draw Loop, loops through argued array and draws each 
-        const circleDrawLoop = (circles,canvas) =>
+        const circleDrawLoop = (circles,c) =>
         {
             // Each loop, every single circle uses the same moveX and moveY, 
             // how do i define movement for each individual circle?
@@ -156,108 +292,41 @@ const shapesModule = (ctx,c) =>
                 current.setY(newY);
 
                 // Checks to see if the x positioning is to the maximum size of the canvas, in both the positive and negative directions
-                if (newX + r > canvas.width || newX - r < 0)
+                if (newX + r > c.width || newX - r < 0)
                 {
                     current.setMoveX(current.getMoveX() * -1);
                 }
                 // Checks Y
-                if (newY + r > canvas.height || newY - r < 0)
+                if (newY + r > c.height || newY - r < 0)
                 {
                     current.setMoveY(current.getMoveY() * -1);
                 }
             }
         }
 
-
-
-onPageLoad = () =>
-{
-    // Clear Button Event Handler for button element in HTML
-    document.getElementById('randomButton').onclick = (event) =>  { clear(); };
-
-    // Declaration of new amounts
-    let newAmt; let newRad;
-    // Declaration of interval object
-    let interval;
-
-    document.getElementById('renderButton').onclick = (event) =>  
-    { 
-        // Clears previous 
-        clearInterval(interval);
-
-        // Receives HTML elements into script
-        amtInput = document.getElementById('amtInput');
-        radInput = document.getElementById('radInput');
-        
-        // Gathers values from elements
-        newAmt = amtInput.value;
-        newRad = radInput.value;
-
-        // Passes new values into initialization script
-        interval = init(newAmt, newRad);
-
-    };
-
-    // Initial Default Page Load
-    interval = init();
-}
-
-// Initialization Script
-const init = (amt,rad) =>
-{
-    // Declaration for 
-    let amtCirc = amt;
-    let radCo = rad;
-
-    // Default. If the two variables are found to be undefined (as with the initial page load), their default values are set to 100 and 50
-    if (!amtCirc && !radCo)
-    {
-        amtCirc = 100; radCo = 50;
-    }
-
-
-    // Init of Canvas
-    const theCanvas = document.getElementById('myCanvas');
-    const theContext = theCanvas.getContext('2d');
-
-    // Resize to Window Viewport on pageload
-    resizeCanvas(theCanvas);
-
-    
-    // Placing Context and Canvas into my shapes modules to encapsulate it for usage with theShapes object instance
-    shapes = shapesModule(theContext,theCanvas);
-
-
-        // Animation Time!!!
-    // Generates Our Circles
-    let circles = genCircles(amtCirc, radCo, shapes,theCanvas);
-
-    // Animate function sets recursive draw() calls
-    const animate = () =>
-    {
-        return setInterval(draw, 10);
-    }
-    
-    // Draws scene once, calls CircleDrawLoop where implementation of bouncing circles is defined
-    const draw = () =>
-    {
-        clear();
-        circleDrawLoop(circles, theCanvas);
-    }
-
-    // let interval = animate();
-
-    // document.getElementById('renderButton').onclick = (event) =>  
-    // { 
-    //     clearInterval(interval);
-    // }
-
-    return animate();
-}
-
-
 // The beginning of the program!!
 onPageLoad();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
